76f3ccd002e0860e923fe5e7569ccb1cb3b06362
Tanay Dherange <tanaydherange@gmail.com>
2026-06-15 14:48:05 +0530

    Assignment 2 question 6
    Write a program that accepts 2 number from user and print their
    >Addition
    >Subtraction
    >Multiplication
    >Division

---
+++ Assignment 2/Assignment-2_Q-6.py
@@ -0,0 +1,31 @@
+print("Enter 1st number = ")
+No1 = int(input())
+
+print("Enter 2nd number = ")
+No2 = int(input())
+
+Ans = No1 + No2
+print("Addition is = ",Ans)
+
+Ans = No1 - No2
+print("Subtraction is = ",Ans)
+
+Ans = No1 * No2
+print("Multiplication is = ",Ans)
+
+Ans = No1 / No2
+print("Division is = ",Ans)
+
+'''
+OutPut :-
+
+Enter 1st number =
+30
+Enter 2nd number =
+6
+Addition is =  36
+Subtraction is =  24
+Multiplication is =  180
+Division is =  5.0
+
+'''
