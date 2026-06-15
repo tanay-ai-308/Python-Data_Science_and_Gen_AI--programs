bea6881a2c7b9b2afbd6038a5f6af2aa9c62c11a
Tanay Dherange <tanaydherange@gmail.com>
2026-06-15 14:49:59 +0530

    Assignment 2 question 9
    Write a program to take user name and age and display :-
    
    "Hello <name> , you will turn <age+1> next year."

---
+++ Assignment 2/Assignment-2_Q-9.py
@@ -0,0 +1,19 @@
+print("Enter your name = ")
+name = input()
+
+print("Enter your age = ")
+age = int(input())
+
+print("Hello",name,",you will turn",age+1,"next year.")
+
+'''
+
+OutPut :-
+
+Enter your name =
+Tanay
+Enter your age =
+21
+Hello Tanay ,you will turn 22 next year.
+
+'''
